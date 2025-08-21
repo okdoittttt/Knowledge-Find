2a0fab0d1a2f24d4351d9380c9ea19e4333fe141
okdoittttt <sonokmoo@gmail.com>
2025-08-21 18:51:38 +0900

    feat: 문서 저장 구현

--- server/main.py
+++ server/main.py
@@ -1,8 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import shutil
+import os
 
-#app test
 app = FastAPI()
+
+UPLOAD_DIRECTORY = "./files"
+os.makedirs(UPLOAD_DIRECTORY, exist_ok=True)
+
 
 @app.get("/")
 def read_root():
-    return {"Hello World"}
+    return {"Hello": "World"}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    """
+    file: 입략받을 파일
+    처음에는 단수로 받아 테스트 한 후, 추후에 복수로 변경해야함.
+    변수명도 복수형으로 변경할 것.
+    """
+    try:
+        file_path = os.path.join(UPLOAD_DIRECTORY, file.filename)
+        
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+            
+    finally:
+        file.file.close()
+
+    return {"filename": file.filename, "message": "파일이 성공적으로 업로드되었습니다."}
