18b57b3f30fa4fe85c84b90a641cfb3f29eab0a0
okdoittttt <sonokmoo@gmail.com>
2025-08-24 23:59:13 +0900

    feat: 유사도 임계값 설정, 중복반환 개선

--- server/search_engine/searchEngine.py
+++ server/search_engine/searchEngine.py
@@ -16,6 +16,7 @@ class SearchEngine:
     def search_vectors(self, qurey_text: str, limit: int = 5):
         '''
         검색 쿼리를 받은 후 Qdrant에서 유사한 벡터를 검색
+        중복된 파일은 유사도가 높은 하나만 반환
         '''
         try:
             query_vector = self.model.encode(qurey_text).tolist()
@@ -27,15 +28,25 @@ class SearchEngine:
                 with_payload=True
             )
 
-            formatted_results = [
+            filtered_results = [
                 {
                     "score": result.score,
                     "word": result.payload.get('word'),
                     "filename": result.payload.get('filename')
                 }
-                for result in search_result
+                for result in search_result if result.score >= 0.4
             ]
-            return {"results": formatted_results}
+
+            # 유사도가 가장 높은 하나의 파일만 반환
+            unique_result = {}
+            for result in filtered_results:
+                filename = result['filename']
+                if filename not in unique_result or result['score'] > unique_result[filename]['score']:
+                    unique_result[filename] = result
+            
+            final_result = list(unique_result.values())
+            
+            return {"results": final_result}
         
         except Exception as e:
             raise HTTPException(status_code=500, detail=f"Search failed: {e}")
